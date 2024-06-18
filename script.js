022eeafc79395810ad778cd2dc39a10079dd565f
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-06-18 18:26:27 +0600

    added

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ menuClose[0].addEventListener("click", () =>{
     menuOpen[0].style.display = "block";
     menuClose[0].style.display = "none" ;
 })
+
+// cart in //
+
+const cartClose = document.getElementById("cart-close");
+const cartOpen = document.getElementById("cart");
+const cartBody = document.getElementById("cartbody");
+
+cartClose.addEventListener("click", () =>{
+    cartBody.style.display = "none";   
+     
+})
+
+cartOpen.addEventListener("click", () => {
+    cartBody.style.display = "block";
+})
+
+// log in //
+
+const logClose = document.getElementById("log-close");
+const logOpen =  document.getElementById("log-in");
+const logInBody =  document.getElementById("loginbody");
+
+logClose.addEventListener("click", () => {
+    logInBody.style.display = "none";
+})
+
+logOpen.addEventListener("click", () => {
+    logInBody.style.display = "block";
+})
